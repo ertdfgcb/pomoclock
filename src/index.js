0d7da7dc8d1997fd59e7c304560ef06ea4ec8817
Paul O'Day <paul@oday.io>
2018-04-18 19:28:12 -0700

    start adding styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import PomoClock from './PomoClock';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<PomoClock work="25" break="5" cycles="1"/>, document.getElementById('root'));
+ReactDOM.render(<App work="25" break="5" cycles="1"/>, document.getElementById('root'));
 registerServiceWorker();
